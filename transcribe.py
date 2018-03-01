299a6e48b40be1be2c92cdcdcbb550dc79710461
Chad Parry <chadparry@indeed.com>
2018-02-28 22:27:26 -0800

    Track hypotheses with a particle filter

--- transcribe.py
+++ transcribe.py
@@ -10,7 +10,15 @@ import subtractor
 import detectmovement
 
 
+Particle = collections.namedtuple('Particle', ['weight', 'board', 'stablelab', 'diffs'])
+
+
+EXPECTED_CORRELATION = 0.3
 HISTORY_LEN = 10
+
+
+def get_particle_key(particle):
+	return particle.board.fen()
 
 
 def main():
@@ -44,13 +52,17 @@ def main():
 	ret, firstrgb = cap.read()
 	if firstrgb is None:
 		return
-	lastmovelab = cv2.cvtColor(firstrgb, cv2.COLOR_BGR2LAB)
+	firstlab = cv2.cvtColor(firstrgb, cv2.COLOR_BGR2LAB)
 
-	frame_size = tuple(reversed(lastmovelab.shape[:-1]))
+	frame_size = tuple(reversed(firstlab.shape[:-1]))
 	heatmaps = detectmovement.get_piece_heatmaps(frame_size, projection)
 	depths = detectmovement.get_depths(projection)
-	board = chess.Board()
-	move_diffs = detectmovement.get_move_diffs(frame_size, heatmaps, depths, board)
+	firstboard = chess.Board()
+	first_move_diffs = detectmovement.get_move_diffs(frame_size, heatmaps, depths, firstboard)
+	first_weight = 1.
+	first_particle = Particle(first_weight, firstboard, firstlab, first_move_diffs)
+	particles = {get_particle_key(first_particle): first_particle}
+	threshold_weight = first_weight / 2
 
 	history = collections.deque()
 	while True:
@@ -65,34 +77,43 @@ def main():
 		if len(history) < HISTORY_LEN:
 			continue
 
-		stablelab = subtractor.get_stable(lastmovelab, history)
-		if lastmovelab is None:
-			stablec = stablelab
-		else:
-			stablec = cv2.absdiff(stablelab, lastmovelab)
-		stablecgray = subtractor.lab2mag(stablec)
+		# FIXME: This needs to be get_stable_mask, because we don't know the lastmovelab!
+		stablelab = subtractor.get_stable(firstlab, history)
 
-		cv2.imshow('frame', stablecgray / 255)
+		# FIXME: The particles are being updated inside this loop
+		for particle in list(particles.values()):
 
-		k = cv2.waitKey(1) & 0xff
-		if k == 27:
-			break
-		if k == ord(' '):
+			stablec = cv2.absdiff(stablelab, particle.stablelab)
+			stablecgray = subtractor.lab2mag(stablec)
 			normalized_subtractor = detectmovement.normalize_diff(stablecgray)
-			best_score = None
-			best_move = None
-			for (move, move_diff) in move_diffs.items():
+			for (move, move_diff) in particle.diffs.items():
 				# The Pearson correlation coefficient measures the goodness of fit
 				score = (move_diff * normalized_subtractor).mean()
-				if best_score is None or score > best_score:
-					best_score = score
-					best_move = move
+				weight = particle.weight * (score + 1 - EXPECTED_CORRELATION)
+				nextboard = particle.board.copy()
+				nextboard.push(move)
+				if weight < threshold_weight:
+					print('  rejected candidate', weight, chess.Board().variation_san(nextboard.move_stack))
+					continue
+				#print('getting diffs');
+				next_move_diffs = detectmovement.get_move_diffs(frame_size, heatmaps, depths, nextboard)
+				#print('got diffs');
+				next_particle = Particle(weight, nextboard, stablelab, next_move_diffs)
+				next_particle_key = get_particle_key(next_particle)
+				existing_particle = particles.get(next_particle_key)
+				if existing_particle is None or existing_particle.weight < next_particle.weight:
+					particles[next_particle_key] = next_particle
+					print('  accepted candidate', weight, chess.Board().variation_san(nextboard.move_stack))
+				else:
+					print('  rejected candidate', weight, chess.Board().variation_san(nextboard.move_stack))
 
-			print('move', board.san(best_move), best_score)
-			board.push(best_move)
-			lastmovelab = stablelab
-			move_diffs = detectmovement.get_move_diffs(frame_size, heatmaps, depths, board)
-
+		# Resample by removing low-weighted particles
+		max_weight = max(particle.weight for particle in particles.values())
+		threshold_weight = max_weight / 2
+		particles = {key: particle for (key, particle) in particles.items() if particle.weight >= threshold_weight}
+		print('particles', len(particles))
+		for particle in sorted(particles.values(), key=lambda particle: particle.weight, reverse=True):
+			print(' ', particle.weight, chess.Board().variation_san(particle.board.move_stack))
 
 	cap.release()
 	cv2.destroyAllWindows()
