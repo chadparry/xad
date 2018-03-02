82b1c837a1bbec8e3f2a7bc1fda8a1ba05fd7304
Chad Parry <chadparry@indeed.com>
2018-03-02 00:40:34 -0800

    Each particle will only generate one child particle per frame

--- transcribe.py
+++ transcribe.py
@@ -42,7 +42,7 @@ def main():
 		'Kxd8',
 		'Bg5',
 		'Be7',
-		'O-O-O',
+		'O-O-O+',
 		'Bd7',
 	]:
 		answer_board.push_san(san)
@@ -115,103 +115,74 @@ def main():
 		# FIXME: The particles are being updated inside this loop
 		# Instead, create a separate collection for the next generation of particles
 		for particle in list(particles.values()):
-
 			frame_diff = cv2.absdiff(framelab, particle.stablelab)
 			stablec = cv2.bitwise_and(frame_diff, stable_mask)
 			stablecgray = subtractor.lab2mag(stablec)
 			#cv2.imshow('frame', stablecgray / 255)
 			#key = cv2.waitKey(1)
 			normalized_subtractor = detectmovement.normalize_diff(stablecgray)
-			best_weight = None
-			best_score = None
-			best_board = None
-			best_move_diff = None
-			for (move, move_diff) in particle.diffs.items():
-				# The Pearson correlation coefficient measures the goodness of fit
-				score = (move_diff * normalized_subtractor).mean()
-				weight = particle.weight * (score + 1 - EXPECTED_CORRELATION)
-				if score < MIN_CORRELATION or weight < threshold_weight:
-					#print('  rejected candidate', weight, chess.Board().variation_san(nextboard.move_stack))
-					continue
-				nextboard = particle.board.copy()
-				nextboard.push(move)
-				next_particle_key = get_board_key(nextboard)
-				existing_particle = particles.get(next_particle_key)
-				if best_weight is None or weight > best_weight:
-					best_weight = weight
-					best_score = score
-					best_board = nextboard
-					best_move_diff = move_diff
-				if existing_particle is None:
-					# Handle after the loop
-					pass
-				elif existing_particle.weight < weight:
-					newstablelab = cv2.bitwise_and(framelab, stable_mask)
-					invmask = cv2.bitwise_not(stable_mask)
-					holelab = cv2.bitwise_and(particle.stablelab, invmask)
-					stablelab = cv2.bitwise_or(holelab, newstablelab)
 
-					next_particle = Particle(weight, nextboard, stablelab, existing_particle.diffs)
-					particles[next_particle_key] = next_particle
-					print('    accepted candidate', score, chess.Board().variation_san(nextboard.move_stack))
-					if weight >= best_weight:
-						best_board = None
-					if next_particle.board.move_stack == ANSWER[:len(next_particle.board.move_stack)]:
-						composite = numpy.zeros((720, 1280, 3), dtype='float32')
-						composite[:,:,2] += move_diff
-						composite[:,:,1] += normalized_subtractor
-						cv2.putText(
-							composite,
-							'SCORE: {:.3f} , MOVE: {}'.format(
-								score,
-								chess.Board().variation_san(nextboard.move_stack),
-							),
-							(10, 40),
-							cv2.FONT_HERSHEY_SIMPLEX,
-							1,
-							(255, 255, 255),
-						)
-						cv2.imshow('frame', composite / 20)
-						cv2.waitKey(1)
-				else:
-					print('    rejected candidate', score, chess.Board().variation_san(nextboard.move_stack))
-					if weight >= best_weight:
-						best_board = None
+			# The Pearson correlation coefficient measures the goodness of fit
+			scores = ((move, move_diff, (move_diff * normalized_subtractor).mean())
+				for (move, move_diff) in particle.diffs.items())
 			# Each particle represents a hypothesis for which frame contains the next move
 			# Particles don't need to represent multiple alternative moves associated with a single frame,
-			# because the piece detection is reliable enough, given an accurate frame hypothesis
-			if best_board is not None:
+			# because the piece detection is reliable enough, given an accurate hypothesis about the timing
+			best_move_item = max(scores, key=lambda item: item[2])
+			(best_move, best_move_diff, best_score) = best_move_item
+
+			weight = particle.weight * (best_score + 1 - EXPECTED_CORRELATION)
+			if best_score < MIN_CORRELATION or weight < threshold_weight:
+				#print('  rejected candidate', weight, chess.Board().variation_san(next_board.move_stack))
+				continue
+
+			newstablelab = cv2.bitwise_and(framelab, stable_mask)
+			invmask = cv2.bitwise_not(stable_mask)
+			holelab = cv2.bitwise_and(particle.stablelab, invmask)
+			stablelab = cv2.bitwise_or(holelab, newstablelab)
+
+			next_board = particle.board.copy()
+			next_board.push(best_move)
+			next_particle_key = get_board_key(next_board)
+			existing_particle = particles.get(next_particle_key)
+
+			if existing_particle is None:
 				print('      getting diffs');
-				best_move_diffs = detectmovement.get_move_diffs(heatmaps, depths, negative_composite_memo, best_board)
+				next_move_diffs = detectmovement.get_move_diffs(heatmaps, depths, negative_composite_memo, next_board)
 				print('      got diffs');
 				print('      negative_composite_memo', len(negative_composite_memo))
+			elif existing_particle.weight < weight:
+				next_move_diffs = existing_particle.diffs
+			else:
+				#print('  rejected candidate', weight, chess.Board().variation_san(next_board.move_stack))
+				continue
 
-				newstablelab = cv2.bitwise_and(framelab, stable_mask)
-				invmask = cv2.bitwise_not(stable_mask)
-				holelab = cv2.bitwise_and(particle.stablelab, invmask)
-				stablelab = cv2.bitwise_or(holelab, newstablelab)
+			next_particle = Particle(weight, next_board, stablelab, next_move_diffs)
+			particles[next_particle_key] = next_particle
+			print('    accepted candidate', best_score, chess.Board().variation_san(next_board.move_stack))
 
-				best_particle = Particle(best_weight, best_board, stablelab, best_move_diffs)
-				best_particle_key = get_board_key(best_board)
-				particles[best_particle_key] = best_particle
-				print('    accepted candidate', best_score, chess.Board().variation_san(best_board.move_stack))
-				if best_particle.board.move_stack == ANSWER[:len(best_particle.board.move_stack)]:
-					composite = numpy.zeros((720, 1280, 3), dtype='float32')
-					composite[:,:,2] += best_move_diff
-					composite[:,:,1] += normalized_subtractor
-					cv2.putText(
-						composite,
-						'SCORE: {:.3f} , MOVE: {}'.format(
-							best_score,
-							chess.Board().variation_san(best_board.move_stack),
-						),
-						(10, 40),
-						cv2.FONT_HERSHEY_SIMPLEX,
-						1,
-						(255, 255, 255),
-					)
-					cv2.imshow('frame', composite / 20)
-					cv2.waitKey(1)
+			if weight > max_weight:
+				composite = numpy.zeros((720, 1280, 3), dtype='float32')
+				composite[:,:,2] += best_move_diff
+				composite[:,:,1] += normalized_subtractor
+				cv2.putText(
+					composite,
+					'SCORE: {:.3f}'.format(best_score),
+					(10, 40),
+					cv2.FONT_HERSHEY_SIMPLEX,
+					1,
+					(255, 255, 255),
+				)
+				cv2.putText(
+					composite,
+					chess.Board().variation_san(next_board.move_stack),
+					(10, 80),
+					cv2.FONT_HERSHEY_SIMPLEX,
+					1,
+					(255, 255, 255),
+				)
+				cv2.imshow('frame', composite / 20)
+				cv2.waitKey(1)
 
 		# Resample by removing low-weighted particles
 		max_weight = max(particle.weight for particle in particles.values())
